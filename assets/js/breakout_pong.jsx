82dd45d38782ba152f4a0b16dcb22f3fd6cd1143
Breakout Pong Game <csstransky@gmail.com>
2020-01-07 20:17:06 -0500

    Changed frontend text of rules regarding points

--- assets/js/breakout_pong.jsx
+++ assets/js/breakout_pong.jsx
@@ -275,9 +275,9 @@ class BreakoutPong extends React.Component {
           <div>
           <h4>Rules:</h4>
           <h5>Use the left and right (or up and down) arrow keys to move your paddle</h5>
-          <h5>Points are awarded for breaking blocks (1 pt) and scoring on your opponent (5 pts)</h5>
-          <h5>You will loose points for letting your own ball pass your own goal line</h5>
-          <h5>The first player to score 20 points will will</h5>
+          <h5>Points are awarded for breaking blocks (1 pt) and scoring on your opponent (10 pts)</h5>
+          <h5>Your opponent will gain points (5 pts) for letting your own ball pass your own goal line</h5>
+          <h5>The first player to score 50 points will win</h5>
 
           </div>
 
